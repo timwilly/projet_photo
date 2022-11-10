250d2bb11ecb69c56439abf7d674726ce40cb154
Willy Tim <willy.tim@outlook.com>
2022-11-10 14:55:28 -0500

    Ajout fonction création de compte

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # Ici on retrouve les objets qui représente les extensions !
 # __name__ : nom du module qui est 'app'. 'app' ici est une instance 
@@ -10,7 +11,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
+# L'url 'login' si n'est pas connecté
+login.login_view = 'login'
 
 # L'importation de routes ici évite le phénomène d'importations
 # circulaires
--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,2 +1,8 @@
 # Importe l'instance de l'application app
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# Crée un contexte shell qui ajoute les instances des données et modèles...
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}

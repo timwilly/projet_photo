11da90f2573bb42b3d0389e7366ada2a2274cf4f
Willy Tim <willy.tim@outlook.com>
2023-02-19 19:51:18 -0500

    ajout triangle dropdown et réglage popup profile

---
+++ microblog/app/static/js/base.js
@@ -0,0 +1,6 @@
+// en commentaire car 'ralenti' l'application et génère des milliers de get...
+/*
+function set_message_count(n) {
+    $('#message_count').text(n);
+    $('#message_count').css('visibility', n ? 'visible' : 'hidden');
+}*/
